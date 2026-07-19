5a0f57d3dba0ecbc60f50d59dd06e77d79d99fb9
kg24ghrt-ops <kg24ghrt@gmail.com>
2026-07-19 14:21:35 +0630

    add fb

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.homecil"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
             cmake {
